41aef16a6d0ab4a6260941f1d1b6f97911112a1c
Chesca <112760526+ches41@users.noreply.github.com>
2023-12-03 02:11:54 +0800

    Fix energy calculation and seam identification

--- seamcarver.py
+++ seamcarver.py
@@ -1,33 +1,43 @@
+import math
+
 #!/usr/bin/env python3
 
 from picture import Picture
 
 class SeamCarver(Picture):
-    ## TO-DO: fill in the methods below
-    def __init__(self, pixels):
-        self.pixels = pixels
     def energy(self, i: int, j: int) -> float:
         '''
         Return the energy of pixel at column i and row j
         '''
         changeEX = self.changeEnergyX(i, j)
         changeEY = self.changeEnergyY(i, j)
-        return changeEX + changeEY
-        raise NotImplementedError
+        return math.sqrt(changeEX + changeEY)
     
     def changeEnergyX(self, i, j):
-        width = len(self.pixels[0])
-        previousPixel = self.pixels[j][(i - 1) % width]
-        nextPixel = self.pixels[j][(i + 1) % width]
+        width = self.width()
+        if (i - 1) < 0:
+            previousPixel = self[(width - 1), j]
+        else:
+            previousPixel = self[(i - 1), j]
+        if (i + 1) <= width:
+            nextPixel = self[0, j]
+        else:
+            nextPixel = self[(i + 1), j]
         redX = abs(nextPixel[0] - previousPixel[0])
         greenX = abs(nextPixel[1] - previousPixel[1])
         blueX = abs(nextPixel[2] - previousPixel[2])
         return redX**2 + greenX**2 + blueX**2
     
     def changeEnergyY(self, i, j):
-        width = len(self.pixels[0])
-        previousPixel = self.pixels[j][(i - 1) % width]
-        nextPixel = self.pixels[j][(i + 1) % width]
+        height = self.height()
+        if (j - 1) < 0:
+            previousPixel = self[i, (height - 1)]
+        else:
+            previousPixel = self[i, (j - 1)]
+        if (j + 1) >= height:
+            nextPixel = self[i, 0]
+        else:
+            nextPixel = self[i, (j + 1)]
         redY = abs(nextPixel[0] - previousPixel[0])
         greenY = abs(nextPixel[1] - previousPixel[1])
         blueY = abs(nextPixel[2] - previousPixel[2])
@@ -48,6 +58,8 @@ class SeamCarver(Picture):
         
         total_energy = [([0] * width) for i in range(height)]
         prev = [([None] * width) for i in range(height)]
+        
+        print(total_energy)
 
         # go through each row
         # store the total energy and previous pixel per pixel
@@ -56,19 +68,30 @@ class SeamCarver(Picture):
             while (column < width):
                 # in first row
                 if (row == 0):
-                    total_energy[0][column] = self.energy()
+                    total_energy[0][column] = self.energy(column, row)
                 # in first column
                 elif (column == 0):
-                    total_energy[row][column] = self.energy() + min(total_energy[row-1][column], total_energy[row-1][column+1])
-                    prev[row][column] = total_energy[row-1].index(min(total_energy[row-1][column], total_energy[row-1][column+1]))
+                    total_energy[row][column] = self.energy(column, row) + min(total_energy[row-1][column], total_energy[row-1][column+1])
+                    if (min(total_energy[row-1][column], total_energy[row-1][column+1]) == total_energy[row-1][column]):
+                        prev[row][column] = column
+                    else:
+                        prev[row][column] = column+1
                 # in last column
                 elif (column == (width-1)):
-                    total_energy[row][column] = self.energy() + min(total_energy[row-1][column-1], total_energy[row-1][column])
-                    prev[row][column] = total_energy[row-1].index(min(total_energy[row-1][column-1], total_energy[row-1][column]))
+                    total_energy[row][column] = self.energy(column, row) + min(total_energy[row-1][column-1], total_energy[row-1][column])
+                    if (min(total_energy[row-1][column-1], total_energy[row-1][column]) == total_energy[row-1][column]):
+                        prev[row][column] = column
+                    else:
+                        prev[row][column] = column-1
                 # everything else
                 else:
-                    total_energy[row][column] = self.energy() + min(total_energy[row-1][column-1], total_energy[row-1][column], total_energy[row-1][column+1])
-                    prev[row][column] = total_energy[row-1].index(min(total_energy[row-1][column-1], total_energy[row-1][column], total_energy[row-1][column+1]))
+                    total_energy[row][column] = self.energy(column, row) + min(total_energy[row-1][column-1], total_energy[row-1][column], total_energy[row-1][column+1])
+                    if (min(total_energy[row-1][column-1], total_energy[row-1][column], total_energy[row-1][column+1]) == total_energy[row-1][column]):
+                        prev[row][column] = column
+                    elif (min(total_energy[row-1][column-1], total_energy[row-1][column], total_energy[row-1][column+1]) == total_energy[row-1][column-1]):
+                        prev[row][column] = column-1
+                    else:
+                        prev[row][column] = column+1
                 column += 1
             column = 0
             row += 1

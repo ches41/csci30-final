d26fdd064c011727b0c51cd71f89e9b59fda6640
arthurlluisma <arthurlluisma03@gmail.com>
2023-12-03 18:42:22 +0800

    Fix remove seam functionality

--- seamcarver.py
+++ seamcarver.py
@@ -14,7 +14,7 @@ class SeamCarver(Picture):
         changeEX = self.changeEnergyX(i, j)
         changeEY = self.changeEnergyY(i, j)
         return math.sqrt(changeEX + changeEY)
-    
+
     def changeEnergyX(self, i, j):
         width = self.width()
         if (i - 1) < 0:
@@ -29,7 +29,7 @@ class SeamCarver(Picture):
         greenX = abs(nextPixel[1] - previousPixel[1])
         blueX = abs(nextPixel[2] - previousPixel[2])
         return redX**2 + greenX**2 + blueX**2
-    
+
     def changeEnergyY(self, i, j):
         height = self.height()
         if (j - 1) < 0:
@@ -50,67 +50,103 @@ class SeamCarver(Picture):
         Return a sequence of indices representing the lowest-energy
         vertical seam
         """
-        
+
         column = 0
         row = 0
         width = self.width()
         height = self.height()
-        
+
         # used https://stackoverflow.com/questions/6667201/how-to-define-a-two-dimensional-array
-        
+
         total_energy = [([0] * width) for i in range(height)]
         prev = [([None] * width) for i in range(height)]
 
         # go through each row
         # store the total energy and previous pixel per pixel
-        
-        while (row < height):
-            while (column < width):
+
+        while row < height:
+            while column < width:
                 # in first row
-                if (row == 0):
+                if row == 0:
                     total_energy[0][column] = self.energy(column, row)
                 # in first column
-                elif (column == 0):
-                    total_energy[row][column] = self.energy(column, row) + min(total_energy[row-1][column], total_energy[row-1][column+1])
-                    if (min(total_energy[row-1][column], total_energy[row-1][column+1]) == total_energy[row-1][column]):
+                elif column == 0:
+                    total_energy[row][column] = self.energy(column, row) + min(
+                        total_energy[row - 1][column], total_energy[row - 1][column + 1]
+                    )
+                    if (
+                        min(
+                            total_energy[row - 1][column],
+                            total_energy[row - 1][column + 1],
+                        )
+                        == total_energy[row - 1][column]
+                    ):
                         prev[row][column] = column
                     else:
-                        prev[row][column] = column+1
+                        prev[row][column] = column + 1
                 # in last column
-                elif (column == (width-1)):
-                    total_energy[row][column] = self.energy(column, row) + min(total_energy[row-1][column-1], total_energy[row-1][column])
-                    if (min(total_energy[row-1][column-1], total_energy[row-1][column]) == total_energy[row-1][column]):
+                elif column == (width - 1):
+                    total_energy[row][column] = self.energy(column, row) + min(
+                        total_energy[row - 1][column - 1], total_energy[row - 1][column]
+                    )
+                    if (
+                        min(
+                            total_energy[row - 1][column - 1],
+                            total_energy[row - 1][column],
+                        )
+                        == total_energy[row - 1][column]
+                    ):
                         prev[row][column] = column
                     else:
-                        prev[row][column] = column-1
+                        prev[row][column] = column - 1
                 # everything else
                 else:
-                    total_energy[row][column] = self.energy(column, row) + min(total_energy[row-1][column-1], total_energy[row-1][column], total_energy[row-1][column+1])
-                    if (min(total_energy[row-1][column-1], total_energy[row-1][column], total_energy[row-1][column+1]) == total_energy[row-1][column]):
+                    total_energy[row][column] = self.energy(column, row) + min(
+                        total_energy[row - 1][column - 1],
+                        total_energy[row - 1][column],
+                        total_energy[row - 1][column + 1],
+                    )
+                    if (
+                        min(
+                            total_energy[row - 1][column - 1],
+                            total_energy[row - 1][column],
+                            total_energy[row - 1][column + 1],
+                        )
+                        == total_energy[row - 1][column]
+                    ):
                         prev[row][column] = column
-                    elif (min(total_energy[row-1][column-1], total_energy[row-1][column], total_energy[row-1][column+1]) == total_energy[row-1][column-1]):
-                        prev[row][column] = column-1
+                    elif (
+                        min(
+                            total_energy[row - 1][column - 1],
+                            total_energy[row - 1][column],
+                            total_energy[row - 1][column + 1],
+                        )
+                        == total_energy[row - 1][column - 1]
+                    ):
+                        prev[row][column] = column - 1
                     else:
-                        prev[row][column] = column+1
+                        prev[row][column] = column + 1
                 column += 1
             column = 0
             row += 1
-        
+
         # find the minimum energy in the last row
         # backtrack through minimals using prev
         # create the vertical seam as you backtrack
-        
+
         vertical_seam = []
-        row = height-2 # start at the second to the last row
-        
-        vertical_seam.insert(0, total_energy[height-1].index(min(total_energy[height-1])))
+        row = height - 2  # start at the second to the last row
+
+        vertical_seam.insert(
+            0, total_energy[height - 1].index(min(total_energy[height - 1]))
+        )
         column = vertical_seam[0]
-        
-        while (row > 0):
+
+        while row > 0:
             vertical_seam.insert(0, prev[row][column])
             column = vertical_seam[0]
             row -= 1
-            
+
         return vertical_seam
 
     def find_horizontal_seam(self) -> list[int]:
@@ -118,89 +154,89 @@ class SeamCarver(Picture):
         Return a sequence of indices representing the lowest-energy
         horizontal seam
         """
-        
+
         # transpose the actual image
-        
+
         og_width = self.width()
         og_height = self.height()
         new_width = self.height()
         new_height = self.width()
-        
+
         # original_image = [([0] * og_width) for i in range(og_height)]
         # transposed_image = [([0] * new_width) for i in range(new_height)]
-        
+
         original_image = {}
         transposed_image = {}
-        
+
         # save old image
-        
+
         column = 0
         row = 0
-        
-        while (row < og_height):
-            while (column < og_width):
+
+        while row < og_height:
+            while column < og_width:
                 original_image[column, row] = self[column, row]
                 column += 1
             column = 0
             row += 1
-            
+
         # create transposed image
-            
+
         column = 0
         row = 0
-            
-        while (row < new_height):
-            while (column < new_width):
-                transposed_image[column, row] = self[row, abs(column-(og_height-1))]
+
+        while row < new_height:
+            while column < new_width:
+                transposed_image[column, row] = self[row, abs(column - (og_height - 1))]
                 column += 1
             column = 0
             row += 1
-            
+
         self.clear()
-        
+
         column = 0
         row = 0
-        
+
         self._width = new_width
         self._height = new_height
-                
-        while (row < new_height):
-            while (column < new_width):
+
+        while row < new_height:
+            while column < new_width:
                 self[column, row] = transposed_image[column, row]
                 column += 1
             column = 0
             row += 1
-        
+
         # run vertical seam on transposed image
-        
+
         horizontal_seam = []
         horizontal_seam = self.find_vertical_seam()
-        
+
         # convert values in horizontal seam
-        
+
         pixel = 1
-        
-        while (pixel < (len(horizontal_seam)-1)):
-            horizontal_seam[pixel] = abs((og_height-1)-horizontal_seam[pixel])
+
+        while pixel < (len(horizontal_seam) - 1):
+            horizontal_seam[pixel] = abs((og_height - 1) - horizontal_seam[pixel])
             pixel += 1
-        
+
         # revert image to original
-        
+
         self.clear()
-        
+
         column = 0
         row = 0
-        
+
         self._width = og_width
         self._height = og_height
-                
-        while (row < og_height):
-            while (column < og_width):
+
+        while row < og_height:
+            while column < og_width:
                 self[column, row] = original_image[column, row]
                 column += 1
             column = 0
             row += 1
-        
+
         return horizontal_seam
 
     def remove_vertical_seam(self, seam: list[int]):
